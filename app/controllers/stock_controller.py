4b39288232d65ea2242582dfae07dbc05bc87e24
etikshajain <jainetiksha@gmail.com>
2024-01-30 15:19:32 +0530

    dynamic search add and remove stocks

--- app/controllers/stock_controller.py
+++ app/controllers/stock_controller.py
@@ -8,39 +8,56 @@ from app import db
 stock = Blueprint("stock", __name__)
 
 stocks = ["AAPL", "GOOGL", "AMZN", "MSFT", "TSLA", "FB"]
+showing_stocks = ["AAPL", "GOOGL", "AMZN", "MSFT", "TSLA", "FB"]
 selected_stocks = []
 graph_stocks = []
 
 @stock.route('/dashboard' , methods=['POST', 'GET'])
 def dashboard():
-    if 'user_id' in session:
-        return render_template('home.html', username=session['username'], all_stocks=stocks, filtered_stocks=stocks, selected_stocks=selected_stocks)
-    else:
-        return redirect(url_for('/.auth.index'))
+    global showing_stocks, selected_stocks, graph_stocks
+    return render_template('home.html', all_stocks=stocks, filtered_stocks=showing_stocks, selected_stocks=selected_stocks, graph_stocks=graph_stocks)
 
-@stock.route('/search', methods=['POST'])
-def search():
-    query = request.form.get('search')
-    results = [stock for stock in stocks if query.lower() in stock.lower()]
-    return render_template('home.html', all_stocks=stocks, filtered_stocks=results, query=query, selected_stocks=selected_stocks)
+@stock.route('/select_stock', methods=['POST'])
+def select_stock():
+    global showing_stocks, selected_stocks, graph_stocks
+    selected_stock = request.form.get('selected_stock')
+    if selected_stock and selected_stock not in selected_stocks:
+        selected_stocks.append(selected_stock)
+    if selected_stock in showing_stocks:
+        showing_stocks.remove(selected_stock)
+    return redirect(url_for('/.stock.dashboard'))
 
-@stock.route('/add_stocks', methods=['POST'])
-def add_stocks():
-    global graph_stocks, selected_stocks
-    added_stocks = request.form.getlist('selected_stocks')
-    selected_stocks.extend(added_stocks)
-    return render_template('home.html', all_stocks=stocks, filtered_stocks=stocks, graph_stocks=graph_stocks, selected_stocks=selected_stocks)
+
+@stock.route('/remove_stock', methods=['POST'])
+def remove_stock():
+    print("here")
+    global showing_stocks, selected_stocks, graph_stocks
+    removed_stock = request.form.get('removed_stock')
+    if removed_stock in selected_stocks:
+        selected_stocks.remove(removed_stock)
+    if removed_stock not in showing_stocks:
+        showing_stocks.append(removed_stock)
+    return redirect(url_for('/.stock.dashboard'))
 
 @stock.route('/update_graph', methods=['POST'])
 def update_graph():
-    global graph_stocks, selected_stocks
+    global graph_stocks, selected_stocks, showing_stocks
     print(selected_stocks)
     graph_stocks = selected_stocks.copy()
-    selected_stocks = []
-    return render_template('home.html', all_stocks=stocks, filtered_stocks=stocks, graph_stocks=graph_stocks, selected_stocks=selected_stocks)
+    # selected_stocks = []
+    return redirect(url_for('/.stock.dashboard'))
 
-# @stock.route('/clear', methods=['POST'])
-# def clear():
-#     graph_stocks = []
-#     selected_stocks = []
-#     return render_template('home.html', all_stocks=stocks, filtered_stocks=stocks, graph_stocks=selected_stocks, selected_stocks=graph_stocks)
+@stock.route('/reset_filter', methods=['POST'])
+def clear():
+    global graph_stocks, selected_stocks, showing_stocks
+    # graph_stocks = []
+    selected_stocks = []
+    showing_stocks = stocks.copy()
+    return redirect(url_for('/.stock.dashboard'))
+
+@stock.route('/show_all_stocks', methods=['POST'])
+def show_all_stocks():
+    global graph_stocks, selected_stocks, showing_stocks
+    # graph_stocks = []
+    showing_stocks = [stock for stock in stocks if stock not in selected_stocks]
+    return redirect(url_for('/.stock.dashboard'))
