1591fa85e5d3c60b00cb4b416a4a9a9559cecd37
etikshajain <jainetiksha@gmail.com>
2024-01-30 18:03:27 +0530

    ajax calls for stock list updations

--- app/controllers/stock_controller.py
+++ app/controllers/stock_controller.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from database.models.user_model import User
 from app import db
+import jsonify
 
 # stock controller blueprint to be registered with api blueprint
 stock = Blueprint("stock", __name__)
@@ -15,29 +16,42 @@ graph_stocks = []
 @stock.route('/dashboard' , methods=['POST', 'GET'])
 def dashboard():
     global showing_stocks, selected_stocks, graph_stocks
-    return render_template('home.html', all_stocks=stocks, filtered_stocks=showing_stocks, selected_stocks=selected_stocks, graph_stocks=graph_stocks)
+    return render_template('home.html', all_stocks=stocks, showing_stocks=showing_stocks, selected_stocks=selected_stocks, graph_stocks=graph_stocks)
 
-@stock.route('/select_stock', methods=['POST'])
+@stock.route('/select_stock', methods=['GET'])
 def select_stock():
     global showing_stocks, selected_stocks, graph_stocks
-    selected_stock = request.form.get('selected_stock')
+    selected_stock = request.args.get('selected_stock')
     if selected_stock and selected_stock not in selected_stocks:
         selected_stocks.append(selected_stock)
     if selected_stock in showing_stocks:
         showing_stocks.remove(selected_stock)
-    return redirect(url_for('/.stock.dashboard'))
+    return {'new_showing_stocks': showing_stocks, 'new_selected_stocks': selected_stocks}
 
 
-@stock.route('/remove_stock', methods=['POST'])
+@stock.route('/remove_stock', methods=['GET'])
 def remove_stock():
-    print("here")
     global showing_stocks, selected_stocks, graph_stocks
-    removed_stock = request.form.get('removed_stock')
+    removed_stock = request.args.get('removed_stock')
     if removed_stock in selected_stocks:
         selected_stocks.remove(removed_stock)
     if removed_stock not in showing_stocks:
         showing_stocks.append(removed_stock)
-    return redirect(url_for('/.stock.dashboard'))
+    return {'new_showing_stocks': showing_stocks, 'new_selected_stocks': selected_stocks}
+
+@stock.route('/reset_filter', methods=['GET'])
+def clear():
+    global graph_stocks, selected_stocks, showing_stocks
+    # graph_stocks = []
+    selected_stocks = []
+    showing_stocks = stocks.copy()
+    return {'new_showing_stocks': showing_stocks, 'new_selected_stocks': selected_stocks}
+
+@stock.route('/show_all_stocks', methods=['GET'])
+def show_all_stocks():
+    global graph_stocks, selected_stocks, showing_stocks
+    showing_stocks = [stock for stock in stocks if stock not in selected_stocks]
+    return {'new_showing_stocks': showing_stocks}
 
 @stock.route('/update_graph', methods=['POST'])
 def update_graph():
@@ -45,19 +59,4 @@ def update_graph():
     print(selected_stocks)
     graph_stocks = selected_stocks.copy()
     # selected_stocks = []
-    return redirect(url_for('/.stock.dashboard'))
-
-@stock.route('/reset_filter', methods=['POST'])
-def clear():
-    global graph_stocks, selected_stocks, showing_stocks
-    # graph_stocks = []
-    selected_stocks = []
-    showing_stocks = stocks.copy()
-    return redirect(url_for('/.stock.dashboard'))
-
-@stock.route('/show_all_stocks', methods=['POST'])
-def show_all_stocks():
-    global graph_stocks, selected_stocks, showing_stocks
-    # graph_stocks = []
-    showing_stocks = [stock for stock in stocks if stock not in selected_stocks]
     return redirect(url_for('/.stock.dashboard'))
--- app/static/js/search.js
+++ app/static/js/search.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($){ 
-    // alert("hello")
+    alert("reloaded")
                 
     $('.live-search-list li').each(function(){ 
     $(this).attr('data-search-term', $(this).text().toLowerCase()); 
@@ -22,3 +22,185 @@ jQuery(document).ready(function($){
     }); 
       
 }); 
+
+function updateSearch(){
+    $('.live-search-list li').each(function(){ 
+        $(this).attr('data-search-term', $(this).text().toLowerCase()); 
+        }); 
+          
+        $('.live-search-box').on('keyup', function(){ 
+          
+        var searchTerm = $(this).val().toLowerCase(); 
+          
+            $('.live-search-list li').each(function(){ 
+          
+                if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) { 
+                    $(this).show(); 
+                } else { 
+                    $(this).hide(); 
+                } 
+          
+            }); 
+          
+        }); 
+}
+
+function selectStock(selectedStock) {
+    // Make an asynchronous GET request to the server
+    fetch(`/stock/select_stock?selected_stock=${selectedStock}`)
+        .then(response => response.json())
+        .then(data => {
+            // Update the content dynamically based on the response
+            const newShowingStocks = data.new_showing_stocks;
+            const newSelectedStocks = data.new_selected_stocks;
+
+            // Update the showing stock list
+            const ul1 = document.querySelector('.live-search-list');
+            ul1.innerHTML = '';
+
+            newShowingStocks.forEach(stock => {
+                const li = document.createElement('li');
+                li.innerHTML = `
+                    <label class="search-list">
+                        ${stock} 
+                        <button type="button" onclick="selectStock('${stock}')">✓</button>
+                    </label>`;
+                ul1.appendChild(li);
+            });
+            updateSearch()
+
+            // Update the selected stock list
+            const ul2 = document.querySelector('.selected-stock-list');
+            ul2.innerHTML = '';
+
+            newSelectedStocks.forEach(stock => {
+                const li = document.createElement('li');
+                li.innerHTML = `
+                    <label class="selected-list">
+                        ${stock} 
+                        <button type="button" onclick="removeStock('${stock}')">X</button>
+                    </label>`;
+                ul2.appendChild(li);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+
+function removeStock(selectedStock) {
+    // Make an asynchronous GET request to the server
+    fetch(`/stock/remove_stock?removed_stock=${selectedStock}`)
+        .then(response => response.json())
+        .then(data => {
+            // Update the content dynamically based on the response
+            const newShowingStocks = data.new_showing_stocks;
+            const newSelectedStocks = data.new_selected_stocks;
+
+            // Update the showing stock list
+            const ul1 = document.querySelector('.live-search-list');
+            ul1.innerHTML = '';
+
+            newShowingStocks.forEach(stock => {
+                const li = document.createElement('li');
+                li.innerHTML = `
+                    <label class="search-list">
+                        ${stock} 
+                        <button type="button" onclick="selectStock('${stock}')">✓</button>
+                    </label>`;
+                ul1.appendChild(li);
+            });
+            updateSearch()
+
+            // Update the selected stock list
+            const ul2 = document.querySelector('.selected-stock-list');
+            ul2.innerHTML = '';
+
+            newSelectedStocks.forEach(stock => {
+                const li = document.createElement('li');
+                li.innerHTML = `
+                    <label class="selected-list">
+                        ${stock} 
+                        <button type="button" onclick="removeStock('${stock}')">X</button>
+                    </label>`;
+                ul2.appendChild(li);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+
+function reset() {
+    // Make an asynchronous GET request to the server
+    fetch(`/stock/reset_filter`)
+        .then(response => response.json())
+        .then(data => {
+            // Update the content dynamically based on the response
+            const newShowingStocks = data.new_showing_stocks;
+            const newSelectedStocks = data.new_selected_stocks;
+
+            // Update the showing stock list
+            const ul1 = document.querySelector('.live-search-list');
+            ul1.innerHTML = '';
+
+            newShowingStocks.forEach(stock => {
+                const li = document.createElement('li');
+                li.innerHTML = `
+                    <label class="search-list">
+                        ${stock} 
+                        <button type="button" onclick="selectStock('${stock}')">✓</button>
+                    </label>`;
+                ul1.appendChild(li);
+            });
+            updateSearch()
+
+            // Update the selected stock list
+            const ul2 = document.querySelector('.selected-stock-list');
+            ul2.innerHTML = '';
+
+            newSelectedStocks.forEach(stock => {
+                const li = document.createElement('li');
+                li.innerHTML = `
+                    <label class="selected-list">
+                        ${stock} 
+                        <button type="button" onclick="removeStock('${stock}')">X</button>
+                    </label>`;
+                ul2.appendChild(li);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+
+function showall() {
+    // Make an asynchronous GET request to the server
+    fetch(`/stock/show_all_stocks`)
+        .then(response => response.json())
+        .then(data => {
+            // Update the content dynamically based on the response
+            const newShowingStocks = data.new_showing_stocks;
+
+            // Update the showing stock list
+            const ul1 = document.querySelector('.live-search-list');
+            ul1.innerHTML = '';
+
+            newShowingStocks.forEach(stock => {
+                const li = document.createElement('li');
+                li.innerHTML = `
+                    <label class="search-list">
+                        ${stock} 
+                        <button type="button" onclick="selectStock('${stock}')">✓</button>
+                    </label>`;
+                ul1.appendChild(li);
+            });
+            updateSearch()
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
