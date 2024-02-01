0e27d0fd3b94a486e1834861e2ddc99ad39603e7
etikshajain <jainetiksha@gmail.com>
2024-02-01 17:02:02 +0530

    btn colors, comments

--- app/controllers/stock_controller.py
+++ app/controllers/stock_controller.py
@@ -20,14 +20,15 @@ graph_stocks = []
 # Creating a client that will send all the API requests to the server.
 api_client = ApiClient()
 
+
 @stock.route('/dashboard' , methods=['POST', 'GET'])
+# Updates the stocks information and Renders the dashboard.
 def dashboard():
     global stocks, showing_stocks, selected_stocks, graph_stocks, stocks_map
-    # Fetch the list of stock symbols from the server.
-    companies_list = api_client.get('all_companies')
     stocks = []
     stocks_map = {}
-    # Update the stocks map to include stock symbols.
+    # Fetch the list of stock symbols from the server Update the stocks map to include stock symbols.
+    companies_list = api_client.get('all_companies')
     for company in companies_list:
         symbol=company['symbol']
         stocks.append(symbol)
@@ -40,12 +41,13 @@ def dashboard():
         stock_obj = api_client.get("statistical_data", {"to_date":end_date,"from_date":start_date}, stock)
         stocks_map[stock]["cagr"] = stock_obj["cagr"]
     
-    # Set the showing list of stocks.
     showing_stocks = [stock for stock in stocks if stock not in selected_stocks]
-    # Render the home page.
     return render_template('home.html', all_stocks=stocks, showing_stocks=showing_stocks, selected_stocks=selected_stocks, graph_stocks=graph_stocks, stocks_map=stocks_map, from_date=start_date, to_date=end_date)
 
+
+
 @stock.route('/process_date' , methods=['POST', 'GET'])
+# Called when the start and end dates are updated.
 def process_date():
     global stocks, showing_stocks, selected_stocks, graph_stocks, stocks_map, start_date, end_date
     start_date = request.form.get('from_date')
@@ -55,7 +57,9 @@ def process_date():
     graph_stocks = []
     return redirect(url_for('/.stock.dashboard'))
 
+
 @stock.route('/filter_stocks', methods=['GET'])
+# Called when the filters are applied.
 def filter_stocks():
     global showing_stocks, selected_stocks, graph_stocks
 
@@ -69,6 +73,7 @@ def filter_stocks():
 
 
 @stock.route('/select_stock', methods=['GET'])
+# Called when a stock is selected.
 def select_stock():
     global showing_stocks, selected_stocks, graph_stocks
     selected_stock = request.args.get('selected_stock')
@@ -80,6 +85,7 @@ def select_stock():
 
 
 @stock.route('/remove_stock', methods=['GET'])
+# Called when a stock is removed.
 def remove_stock():
     global showing_stocks, selected_stocks, graph_stocks
     removed_stock = request.args.get('removed_stock')
@@ -89,25 +95,32 @@ def remove_stock():
         showing_stocks.append(removed_stock)
     return {'new_showing_stocks': showing_stocks, 'new_selected_stocks': selected_stocks, 'stocks_map': stocks_map}
 
+
 @stock.route('/reset_filter', methods=['GET'])
+# Called when reset button is clicked.
 def clear():
     global graph_stocks, selected_stocks, showing_stocks
     selected_stocks = []
     showing_stocks = stocks.copy()
     return {'new_showing_stocks': showing_stocks, 'new_selected_stocks': selected_stocks, 'stocks_map': stocks_map}
 
+
 @stock.route('/show_all_stocks', methods=['GET'])
+# Called when the show all button is clicked.
 def show_all_stocks():
     global graph_stocks, selected_stocks, showing_stocks
     showing_stocks = [stock for stock in stocks if stock not in selected_stocks]
     return {'new_showing_stocks': showing_stocks, 'stocks_map': stocks_map}
 
+
 @stock.route('/update_graph', methods=['GET'])
+# Called when the Update Graph button is clicked.
 def update_graph():
     global graph_stocks, selected_stocks, showing_stocks, start_date, end_date
     graph_stocks = selected_stocks.copy()
     stockDataArray = []
     for stock in graph_stocks:
+        # Fetch the market data for the stock from the server.
         stockData = api_client.get('market_data',{"to_date":end_date,"from_date":start_date}, stock)
         stockDataArray.append({'stock':stock, 'data':stockData})
     return {'stockDataArray': stockDataArray}
